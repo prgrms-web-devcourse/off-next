57ff3bb24534eed2e6b0c93251669578f878cb81
jgjgill <dbdltm22@naver.com>
2023-10-06 16:44:41 +0900

    feat: next 및 off-design-system 설치

---
+++ app/page.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import { JGButton } from "off-design-system";
+
+export default function Home() {
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <JGButton>jgjgill</JGButton>
+    </main>
+  );
+}
